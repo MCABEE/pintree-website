b21b2f0a9a131e99e63e86153407d4ad17f0dc70
jisha-mcabee <jisha@mcabee.in>
2025-12-18 18:06:25 +0530

    Create responsive design for newsroom page

--- src/app/newsroom/page.js
+++ src/app/newsroom/page.js
@@ -6,7 +6,7 @@ function page() {
   return (
     <>
       <Navbar />
-      <div className="px-[24px] sm:px-[240px] max-h-max ">
+      <div className=" px-[24px] 2xl:px-[140px] 3xl:px-[240px]  max-h-max ">
         <h1 className="text-[48px] sm:text-[96px] font-aeonik-medium text-[#1A1A1A] pb-[25px] text-left xl:text-center pt-[100px] md:pt-[180px]">
           Newsroom
         </h1>
@@ -15,18 +15,27 @@ function page() {
           <br />
           shared directly with our community and media.
         </p>
-        <h1 className="text-[40px] text-[#0C0C0C] pt-[187px]">Latest</h1>
-        <div className="flex gap-10">
-          <img src="/news1.svg" alt="" className="w-[600px] h-[338px]" />
+        <h1 className="text-[24px] sm:text-[40px] text-[#0C0C0C] pt-[135px] sm:pt-[187px] pb-2">
+          Latest
+        </h1>
+        <div className="flex flex-col 2xl:flex-row 2xl:gap-10">
+          <p className="text-[20px] text-[#000000] pb-3.5 2xl:hidden inline-block ">
+            15 December 2025, Mon
+          </p>
+          <p className="text-[24px] sm:text-[40px] text-[#0C0C0C] leading-[120%] pb-0.5 2xl:hidden inline-block ">
+            Pintree Introduces New Community Features to Strengthen Meaningful
+            Connections.
+          </p>
+          <img src="/news1.svg" alt="" className="2xl:w-[600px] h-[338px]" />
           <div>
-            <p className="text-[20px] text-[#000000] pb-3.5">
+            <p className="text-[20px] text-[#000000] pb-3.5 hidden 2xl:inline-block ">
               15 December 2025, Mon
             </p>
-            <p className="text-[40px] text-[#0C0C0C] leading-[120%] pb-0.5">
+            <p className="text-[24px] sm:text-[40px] text-[#0C0C0C] leading-[120%] pb-0.5 hidden 2xl:inline-block ">
               Pintree Introduces New Community Features to Strengthen Meaningful
               Connections.
             </p>
-            <p className="text-[20px] text-[#0C0C0C] leading-[140%]">
+            <p className="text-[16px] sm:text-[20px] text-[#0C0C0C] leading-[140%]">
               Pintree has rolled out new community-focused features designed to
               help users connect around shared interests more effectively. The
               update enhances discovery, interaction, and moderation tools,
@@ -40,44 +49,44 @@ function page() {
           <p className="pt-10 text-[#0C0C0C] text-[40px] pb-[22px]">
             More from Pintree
           </p>
-          <div className="flex gap-6 pb-28 ">
-            <div className="flex flex-col w-[464px] ">
+          <div className="flex flex-col xl:flex-row   gap-6 pb-28 ">
+            <div className="flex flex-col xl:w-[464px] ">
               <img src="/news2.svg" alt="" />
-              <p className="text-[20px] text-[#000000] pb-3.5 pt-[20px]">
+              <p className="text-[16px] sm:text-[20px] text-[#000000] pb-3.5 pt-[20px]">
                 09 December 2025, Tue
               </p>
-              <p className="text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
+              <p className="text-[24px] sm:text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
                 Pintree Expands Community Tools Across the Platform
               </p>
-              <p className="text-[20px] text-[#0C0C0C] leading-[140%]">
+              <p className="text-[16px] sm:text-[20px] text-[#0C0C0C] leading-[140%]">
                 New tools help users create, manage, and engage in
                 purpose-driven communities more easily.
               </p>
               <p className="text-[#0453F1] pt-4">Read More</p>
             </div>
-            <div className="flex flex-col w-[464px]">
+            <div className="flex flex-col xl:w-[464px]">
               <img src="/news2.svg" alt="" />
-              <p className="text-[20px] text-[#000000] pb-3.5 pt-[20px]">
+              <p className="text-[16px] sm:text-[20px] text-[#000000] pb-3.5 pt-[20px]">
                 09 December 2025, Tue
               </p>
-              <p className="text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
+              <p className="text-[24px] sm:text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
                 Pintree Expands Community Tools Across the Platform
               </p>
-              <p className="text-[20px] text-[#0C0C0C] leading-[140%]">
+              <p className="text-[16px] sm:text-[20px] text-[#0C0C0C] leading-[140%]">
                 New tools help users create, manage, and engage in
                 purpose-driven communities more easily.
               </p>
               <p className="text-[#0453F1] pt-4">Read More</p>
             </div>
-            <div className="flex flex-col w-[464px]">
+            <div className="flex flex-col xl:w-[464px]">
               <img src="/news2.svg" alt="" />
-              <p className="text-[20px] text-[#000000] pb-3.5 pt-[20px]">
+              <p className="text-[16px] sm:text-[20px] text-[#000000] pb-3.5 pt-[20px]">
                 09 December 2025, Tue
               </p>
-              <p className="text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
+              <p className="text-[24px] sm:text-[40px] text-[#0C0C0C] leading-[120%] pb-3">
                 Pintree Expands Community Tools Across the Platform
               </p>
-              <p className="text-[20px] text-[#0C0C0C] leading-[140%]">
+              <p className="text-[16px] sm:text-[20px] text-[#0C0C0C] leading-[140%]">
                 New tools help users create, manage, and engage in
                 purpose-driven communities more easily.
               </p>
@@ -86,6 +95,7 @@ function page() {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
